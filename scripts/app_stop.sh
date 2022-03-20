d426bcb6e53b4919f5fd61b6ff7f695f0e3c5cfd
vietba2104 <vietba2104.feit21040@gmail.com>
2022-03-20 07:02:16 +0700

    update appspec.yml

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "Stop httpd "
-sudo chmod -R 777 /var/www/html
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-systemctl stop httpd
+# echo "Stop httpd "
+# sudo chmod -R 777 /var/www/html
+# yum install httpd -y
+# systemctl start httpd
+# systemctl enable httpd
+# systemctl stop httpd
+
+echo "<h1> Hello CIICD </h1>" > /var/www/html/index2.html
 
